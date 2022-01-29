70ebcb72b7ca1a62406baa3d77731d62bafd87cc
nph278 <carllegrone@protonmail.com>
2022-01-29 17:53:43 -0500

    apparmor

--- system/configuration.nix
+++ system/configuration.nix
@@ -300,6 +300,10 @@ in
     jetbrains-mono
   ];
 
+  security.apparmor = {
+    enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

7a031364022efaad992a0ac3058e93bd706bd027
nph278 <carllegrone@protonmail.com>
2022-01-30 08:51:38 -0500

    sway shortcuts

--- system/configuration.nix
+++ system/configuration.nix
@@ -92,6 +92,7 @@ in
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
+  networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [  ];
   networking.firewall.allowedUDPPorts = [  ];
   # Or disable the firewall altogether.
@@ -107,8 +108,6 @@ in
       enable = true;
       wrapperFeatures.gtk = true;
       config = {
-        modifier = "Mod4";
-        terminal = "alacritty";
         fonts = swayFont;
         gaps.inner = 5;
 
@@ -145,6 +144,71 @@ in
             };
           };
         }];
+
+        keybindings = 
+          let
+            mod = "Mod4";
+            term = "alacritty";
+            menu = "${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
+          in
+          {
+            "${mod}+Shift+q" = "kill";
+
+            "${mod}+h" = "focus left";
+            "${mod}+j" = "focus down";
+            "${mod}+k" = "focus up";
+            "${mod}+l" = "focus right";
+
+            "${mod}+Shift+h" = "move left";
+            "${mod}+Shift+j" = "move down";
+            "${mod}+Shift+k" = "move up";
+            "${mod}+Shift+l" = "move right";
+
+            "${mod}+b" = "splith";
+            "${mod}+v" = "splitv";
+            "${mod}+f" = "fullscreen toggle";
+            "${mod}+a" = "focus parent";
+
+            "${mod}+s" = "layout stacking";
+            "${mod}+w" = "layout tabbed";
+            "${mod}+e" = "layout toggle split";
+
+            "${mod}+Shift+space" = "floating toggle";
+            "${mod}+space" = "focus mode_toggle";
+
+            "${mod}+1" = "workspace number 1";
+            "${mod}+2" = "workspace number 2";
+            "${mod}+3" = "workspace number 3";
+            "${mod}+4" = "workspace number 4";
+            "${mod}+5" = "workspace number 5";
+            "${mod}+6" = "workspace number 6";
+            "${mod}+7" = "workspace number 7";
+            "${mod}+8" = "workspace number 8";
+            "${mod}+9" = "workspace number 9";
+
+            "${mod}+Shift+1" = "move container to workspace number 1";
+            "${mod}+Shift+2" = "move container to workspace number 2";
+            "${mod}+Shift+3" = "move container to workspace number 3";
+            "${mod}+Shift+4" = "move container to workspace number 4";
+            "${mod}+Shift+5" = "move container to workspace number 5";
+            "${mod}+Shift+6" = "move container to workspace number 6";
+            "${mod}+Shift+7" = "move container to workspace number 7";
+            "${mod}+Shift+8" = "move container to workspace number 8";
+            "${mod}+Shift+9" = "move container to workspace number 9";
+
+            "${mod}+Shift+minus" = "move scratchpad";
+            "${mod}+minus" = "scratchpad show";
+
+            "${mod}+Shift+c" = "reload";
+            "${mod}+Shift+e" =
+              "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+            "${mod}+r" = "mode resize";
+
+            "${mod}+Return" = "exec ${term}";
+            "${mod}+d" = "exec ${menu}";
+            "${mod}+q" = "exec qutebrowser";
+        };
       };
     };
 

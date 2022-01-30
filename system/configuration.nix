cb26308e060d637f1e57d80caec25921e519ed38
nph278 <carllegrone@protonmail.com>
2022-01-30 11:00:10 -0500

    fix swaylock

--- system/configuration.nix
+++ system/configuration.nix
@@ -585,6 +585,8 @@ in
     enable = true;
   };
 
+  security.pam.services.swaylock = {};
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

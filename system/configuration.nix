890d7d351221dd01c33dd02fa89b8fdd4f0cdd9b
nph278 <carllegrone@protonmail.com>
2022-02-13 09:17:12 -0500

    wtf is wrong with swaylock

--- system/configuration.nix
+++ system/configuration.nix
@@ -39,6 +39,11 @@ let
     names = [font];
     size = 10.0;
   };
+
+  mod = "Mod4";
+  term = "alacritty";
+  menu = "${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
+  lock = "${pkgs.swaylock}/bin/swaylock -f -c #000000";
 in
 {
   imports =
@@ -171,77 +176,71 @@ in
           };
         }];
 
-        keybindings = 
-          let
-            mod = "Mod4";
-            term = "alacritty";
-            menu = "${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
-          in
-          {
-            "${mod}+Shift+q" = "kill";
+        keybindings = {
+          "${mod}+Shift+q" = "kill";
 
-            "${mod}+h" = "focus left";
-            "${mod}+j" = "focus down";
-            "${mod}+k" = "focus up";
-            "${mod}+l" = "focus right";
+          "${mod}+h" = "focus left";
+          "${mod}+j" = "focus down";
+          "${mod}+k" = "focus up";
+          "${mod}+l" = "focus right";
 
-            "${mod}+Shift+h" = "move left";
-            "${mod}+Shift+j" = "move down";
-            "${mod}+Shift+k" = "move up";
-            "${mod}+Shift+l" = "move right";
+          "${mod}+Shift+h" = "move left";
+          "${mod}+Shift+j" = "move down";
+          "${mod}+Shift+k" = "move up";
+          "${mod}+Shift+l" = "move right";
 
-            "${mod}+b" = "splith";
-            "${mod}+v" = "splitv";
-            "${mod}+f" = "fullscreen toggle";
-            "${mod}+a" = "focus parent";
+          "${mod}+b" = "splith";
+          "${mod}+v" = "splitv";
+          "${mod}+f" = "fullscreen toggle";
+          "${mod}+a" = "focus parent";
 
-            "${mod}+s" = "layout stacking";
-            "${mod}+w" = "layout tabbed";
-            "${mod}+e" = "layout toggle split";
+          "${mod}+s" = "layout stacking";
+          "${mod}+w" = "layout tabbed";
+          "${mod}+e" = "layout toggle split";
 
-            "${mod}+Shift+space" = "floating toggle";
-            "${mod}+space" = "focus mode_toggle";
+          "${mod}+Shift+space" = "floating toggle";
+          "${mod}+space" = "focus mode_toggle";
 
-            "${mod}+1" = "workspace number 1";
-            "${mod}+2" = "workspace number 2";
-            "${mod}+3" = "workspace number 3";
-            "${mod}+4" = "workspace number 4";
-            "${mod}+5" = "workspace number 5";
-            "${mod}+6" = "workspace number 6";
-            "${mod}+7" = "workspace number 7";
-            "${mod}+8" = "workspace number 8";
-            "${mod}+9" = "workspace number 9";
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
 
-            "${mod}+Shift+1" = "move container to workspace number 1";
-            "${mod}+Shift+2" = "move container to workspace number 2";
-            "${mod}+Shift+3" = "move container to workspace number 3";
-            "${mod}+Shift+4" = "move container to workspace number 4";
-            "${mod}+Shift+5" = "move container to workspace number 5";
-            "${mod}+Shift+6" = "move container to workspace number 6";
-            "${mod}+Shift+7" = "move container to workspace number 7";
-            "${mod}+Shift+8" = "move container to workspace number 8";
-            "${mod}+Shift+9" = "move container to workspace number 9";
+          "${mod}+Shift+1" = "move container to workspace number 1";
+          "${mod}+Shift+2" = "move container to workspace number 2";
+          "${mod}+Shift+3" = "move container to workspace number 3";
+          "${mod}+Shift+4" = "move container to workspace number 4";
+          "${mod}+Shift+5" = "move container to workspace number 5";
+          "${mod}+Shift+6" = "move container to workspace number 6";
+          "${mod}+Shift+7" = "move container to workspace number 7";
+          "${mod}+Shift+8" = "move container to workspace number 8";
+          "${mod}+Shift+9" = "move container to workspace number 9";
 
-            "${mod}+Shift+minus" = "move scratchpad";
-            "${mod}+minus" = "scratchpad show";
+          "${mod}+Shift+minus" = "move scratchpad";
+          "${mod}+minus" = "scratchpad show";
 
-            "${mod}+Shift+c" = "reload";
-            "${mod}+Shift+e" =
-              "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+          "${mod}+Shift+c" = "reload";
+          "${mod}+Shift+e" =
+            "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
 
-            "${mod}+r" = "mode resize";
+          "${mod}+r" = "mode resize";
 
-            "${mod}+Return" = "exec ${term}";
-            "${mod}+d" = "exec ${menu}";
-            "${mod}+q" = "exec qutebrowser";
-            "${mod}+Control+l" = "exec \"swaylock -f -c #000000\""; # Why doesnt this work?
+          "${mod}+Return" = "exec ${term}";
+          "${mod}+d" = "exec ${menu}";
+          "${mod}+q" = "exec qutebrowser";
+          "${mod}+Control+l" = "exec \"${lock}\""; # Why doesnt this work?
         };
       };
       extraConfig = ''
         exec swayidle -w \
-          timeout 300 'swaylock -f -c #000000 ' \
+          timeout 300 '${lock} ' \
           timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-          before-sleep 'swaylock -f -c #000000'
+          before-sleep '${lock}'
       '';
     };
 

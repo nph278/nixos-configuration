ffce889d18d5493609bf7d410e8410d0c27b2d1a
nph278 <carllegrone@protonmail.com>
2022-08-19 12:43:34 -0400

    fix p alias

--- users/carl/z_shell.nix
+++ users/carl/z_shell.nix
@@ -44,7 +44,7 @@ in
     dev = "toolbox run -c dev zsh";
     ssh-setup = "killall ssh-agent; kee && eval \"$(ssh-agent -s)\" && wl-paste | ssh-add ~/.ssh/id_ed25519 && wl-copy ''";
     lock = "${import ./swaylock.nix {inherit pkgs; }}";
-    p = "cd $(/usr/bin/env ls ~/Projects | grep -v '\\\.[^i]' | fzf)";
+    p = "cd ~/Projects/$(/usr/bin/env ls ~/Projects | grep -v '\\\.[^i]' | fzf)";
     down = "(rebuild --upgrade && shutdown now) || lock";
     garbage = "doas nix-collect-garbage -d";
     redo = "$(cat ~/.zsh_history | fzf)";

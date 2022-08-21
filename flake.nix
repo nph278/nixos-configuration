736234cbca539b0387d13dede9dfe1be18b69e06
nph278 <carllegrone@protonmail.com>
2022-08-21 12:45:26 -0400

    quote input urls

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "My NixOS / home-manager configuration";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-  inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  inputs.home-manager.url = github:nix-community/home-manager/release-22.05;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-22.05";
 
   outputs = { nixpkgs, ... }@attrs: {
     nixosConfigurations.tp01 = nixpkgs.lib.nixosSystem {

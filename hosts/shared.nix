b608403ac3e61f8870a49f58c67a9a9728a5d8ee
nph278 <carllegrone@protonmail.com>
2022-09-07 17:13:54 -0400

    nix-community cachix

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -57,6 +57,15 @@
   };
 
   # Nix settings
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   nix.allowedUsers = [ "@wheel" ];
 }

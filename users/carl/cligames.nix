79a6b412bffcbfe83df2b0cef4fd1f228fe3d9c3
nph278 <carllegrone@protonmail.com>
2022-09-09 08:14:49 -0400

    slow_snake

--- users/carl/cligames.nix
+++ users/carl/cligames.nix
@@ -15,8 +15,8 @@ in
         src = pkgs.fetchFromGitHub {
           owner = "nph278";
           repo = "snake-crossterm";
-          rev = "226546ea372c075555c7a44649a84bc64627361c";
-          sha256 = "74fe9ae67cf9e397fc08eed6547739abbc917639b11535b6f780baa80f968ab5";
+          rev = "710d6dcb1c13df9e5f2c6bb87c27cea1b4901c2b";
+          sha256 = "738aa47796b30610d5bc492b4bf4dc8f90fc5f291df7230c78e57aee79c00804";
         };
 
         cargoSha256 = "0nr7pv66dchhy1vsfhx41p56svwfms7qxhbxb3s7pgrfihdjshas";

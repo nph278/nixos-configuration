6f7a04196bc66bea3b76a8606fd5485ddb004ad7
nph278 <carllegrone@protonmail.com>
2022-10-21 19:16:10 -0400

    prolog modules

--- users/carl/prolog/default.nix
+++ users/carl/prolog/default.nix
@@ -5,5 +5,8 @@
 
   home.file.".scryerrc".text = ''
     :- use_module(library(lists)).
+    :- use_module(library(clpz)).
+    :- use_module(library(dcgs)).
+    :- use_module(library(reif)).
   '';
 }
